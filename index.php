bdb66221178262c535826e5429df0d3980ee1bfb
yogesh2312-more <62593133+yogesh2312-more@users.noreply.github.com>
2021-03-04 12:52:40 +0530

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ $conn = mysqli_connect("localhost","root","emp","test");
 
 // Check connection
 if (mysqli_connect_errno()) {
-  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  echo "Failed: " . mysqli_connect_error();
   exit();
 }
 
